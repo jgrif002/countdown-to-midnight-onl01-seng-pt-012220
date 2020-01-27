098797bab726bdc6a39892fd79615827e0baca0f
Jesse Griffin <jesse.griffin@gmail.com>
2020-01-27 06:19:57 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -5,6 +5,7 @@ def countdown(number)
     puts "#{number} SECOND(S)!"
     number -= 1
   end
+  return "HAPPY NEW YEAR!" if number == 0
 end
 
 def countdown_with_sleep(number)
@@ -13,4 +14,5 @@ def countdown_with_sleep(number)
     sleep(1)
     number -= 1
   end
+  return "HAPPY NEW YEAR!" if number == 0
 end
